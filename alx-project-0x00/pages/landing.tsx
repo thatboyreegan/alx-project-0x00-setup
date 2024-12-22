39d95e8b8a01ff8f32bc890545aaeaaf183ad3d6
thatboyreegan <reaganronald491@gmail.com>
2024-12-22 14:21:15 +0300

    add: Rendering components

---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,14 @@
+import Card from "@/components/Card"
+import Pill from "@/components/Pill"
+
+const Landing: React.FC =  () => {
+    return (
+      <div>
+        <h1 className=" text-xl font-extralight">Landing Page</h1>
+        <Card/>
+    
+      </div>
+    )
+  }
+  export default Landing
+  
